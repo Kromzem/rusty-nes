afbb6e890f1e186e7c288907c3f4db4cea7d9253
kromzem <kromzem@tutamail.com>
2024-04-17 21:16:58 +0200

    some structure

---
+++ engine/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn get_greeting(to_greet: &str) -> String {
+    format!("Hello, {to_greet}!")
+}
---
+++ tui/src/main.rs
@@ -0,0 +1,7 @@
+use engine::get_greeting;
+
+fn main() {
+    let greeting = get_greeting("you");
+
+    println!("{greeting}");
+}
